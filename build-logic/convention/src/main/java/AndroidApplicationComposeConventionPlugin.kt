053d38a93c1259a5bd6bb109546691903756e96c
HyungwonJin <hwjin92@gmail.com>
2025-04-01 09:55:23 +0900

    feat: apply Kotlin Compose plugin to Android application and library conventions

--- build-logic/convention/src/main/java/AndroidApplicationComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationComposeConventionPlugin.kt
@@ -4,11 +4,12 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.getByType
 
-class AndroidApplicationComposeConventionPlugin: Plugin<Project> {
+class AndroidApplicationComposeConventionPlugin : Plugin<Project> {
 
   override fun apply(target: Project) {
     target.run {
       pluginManager.apply("application.android.application")
+      pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
 
       val extension = extensions.getByType<ApplicationExtension>()
       configureAndroidCompose(extension)
