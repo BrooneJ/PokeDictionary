26a1cf73b2504e24f0111f5cb927d28bedc7b522
HyungwonJin <hwjin92@gmail.com>
2025-04-06 10:04:34 +0900

    feat: add image loading for Pokemon with error handling

--- content/presentation/src/main/java/com/example/content/presentation/home/model/PokemonUi.kt
+++ content/presentation/src/main/java/com/example/content/presentation/home/model/PokemonUi.kt
@@ -4,4 +4,11 @@ data class PokemonUi(
   var page: Int = 0,
   val nameField: String,
   val url: String,
-)
+) {
+
+  val imageUrl: String
+    get() {
+      val index = url.split("/".toRegex()).dropLast(1).last()
+      return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png"
+    }
+}
--- core/domain/src/main/java/com/example/core/domain/content/Pokemon.kt
+++ core/domain/src/main/java/com/example/core/domain/content/Pokemon.kt
@@ -8,4 +8,10 @@ data class Pokemon(
 
   val name: String
     get() = nameField.replaceFirstChar { it.uppercase() }
+
+  val imageUrl: String
+    get() {
+      val index = url.split("/".toRegex()).dropLast(1).last()
+      return "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$index.png"
+    }
 }
