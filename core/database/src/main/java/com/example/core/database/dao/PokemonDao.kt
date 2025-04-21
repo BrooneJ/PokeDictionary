af4e35c6496bf9851510d8beca936f9420109d6e
HyungwonJin <hwjin92@gmail.com>
2025-04-21 14:36:43 +0900

    feat: add query methods to retrieve Pokemon entities from database

--- core/database/src/main/java/com/example/core/database/dao/PokemonDao.kt
+++ core/database/src/main/java/com/example/core/database/dao/PokemonDao.kt
@@ -3,6 +3,7 @@ package com.example.core.database.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import com.example.core.database.entity.PokemonEntity
 
 @Dao
@@ -10,4 +11,10 @@ interface PokemonDao {
 
   @Insert(onConflict = OnConflictStrategy.REPLACE)
   suspend fun insertPokemon(pokemonList: List<PokemonEntity>)
+
+  @Query("SELECT * FROM PokemonEntity WHERE page = :page_")
+  suspend fun getPokemonList(page_: Int): List<PokemonEntity>
+
+  @Query("SELECT * FROM PokemonEntity WHERE page <= :page_")
+  suspend fun getAllPokemonList(page_: Int): List<PokemonEntity>
 }
